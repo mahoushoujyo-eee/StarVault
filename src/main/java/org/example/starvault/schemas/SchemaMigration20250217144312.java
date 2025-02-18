4f77fa7620fbf02f33fd65ccebbbdcd2b7c9686d
mahoushoujyo-eee <1653545106@qq.com>
2025-02-18 14:41:15 +0800

    完成基本的用户账号管理功能

---
+++ src/main/java/org/example/starvault/schemas/SchemaMigration20250217144312.java
@@ -0,0 +1,15 @@
+package org.example.starvault.schemas;
+
+import stark.coderaider.fluentschema.commons.schemas.ColumnMetadata;
+import stark.coderaider.fluentschema.commons.schemas.KeyMetadata;
+import stark.coderaider.fluentschema.commons.schemas.SchemaMigrationBase;
+import java.util.List;
+
+public class SchemaMigration20250217144312 extends SchemaMigrationBase {
+    @Override
+    public void forward() {
+    }
+    @Override
+    public void backward() {
+    }
+}
